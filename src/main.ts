24aa3975e74694d73af3dafc3c8a7af94c6dfb39
@Alnkle621 <kaiquemendesilva82@gmail.com>
2023-07-09 18:18:32 -0300

    add filters

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './utils/filters/bad-request';
+import { InterceptorResponse } from './utils/filters/interceptor-response';
+import { ResultExceptionFilter } from './utils/filters/result-request';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalFilters(new ResultExceptionFilter());
+  app.useGlobalInterceptors(new InterceptorResponse());
   await app.listen(3000);
 }
 bootstrap();
